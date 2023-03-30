c71242ad138561b4f4ca72c03d6a4ca3cd443211
Victor Martinsson <victor.martinsson@gmail.com>
2023-03-30 11:46:44 +0200

    Pi and parameters

---
+++ control.py
@@ -0,0 +1,27 @@
+class PI:
+
+    def __init__(self, pi_paramters):
+        self._p = pi_paramters
+        self._I = 0
+        self._v = 0
+        self._e_i = 0
+    
+    def calculate_output(self, y_i, ref_i):
+        self._e_i = ref_i - y_i
+        v = self._p.K * (self._p.beta * (ref_i - y_i)) + self._I
+        return v
+
+
+    def update_state(self, u):
+        self._I += (self._p.K*self._p.h / self._p.Ti)*self._e_i + (self._p.h / self._p.Tr)*(u - self._v)
+     
+class PIParameters:
+    
+    def __init__(self, K, Ti, h, beta, Tr):
+        self.K = K
+        self.Ti = Ti
+        self.h = h
+        self.beta = beta
+        self.Tr = Tr
+
+    
