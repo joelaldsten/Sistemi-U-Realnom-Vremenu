037dfa3b76e74c3276d383d2efb04bbd65ce642b
Victor Martinsson <victor.martinsson@gmail.com>
2023-03-30 13:48:10 +0200

    heh

--- position.py
+++ position.py
@@ -1,5 +1,12 @@
-import cflib.positioning as pos
+import cflib.crazyflie
+import cflib.positioning.position_hl_commander
+from cflib.utils import uri_helper
 import time
+
+cf = Crazyflie(rw_cache='./cache')
+link_uri = uri_helper.uri_from_env(default='usb://0')
+cf.open_link(link_uri)
+pos = PositionHlCommander(cf)
 
 while True:
     print(pos.get_position())
