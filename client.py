bccf7341e449210bf8eaaf602fd9fa62d7243a03
joelaldsten <aldsten.joel@gmail.com>
2023-04-26 14:38:10 +0200

    xddddddddddddddddd gui com huahgfasufigaseiytfvajkrvh ouiyawdyuaesdfuiasefuiyfaseyfugsarhiogj

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ import socket
 
 s = socket.socket()
 s.connect(("kanelbulle.duckdns.org", 55555))
-s.sendall(b'Hello, world')
-data = s.recv(1024)
-print('Received', repr(data))
+while True:
+    data = s.recv(1024)
+    print('Received', repr(data))
