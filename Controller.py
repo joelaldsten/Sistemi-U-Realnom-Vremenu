59150c439bdc572f5c39fc6904d285d4017e1545
Victor Martinsson <victor.martinsson@gmail.com>
2023-04-26 14:41:47 +0200

    ändrade param

--- Controller.py
+++ Controller.py
@@ -164,7 +164,7 @@ uri = uri_helper.uri_from_env(default='usb://0') # Connection-uri for crazyflie 
 cl = CrazyLogger(uri)
 time.sleep(1) # Wait for connection to work
 
-piParam = PIParameters(10,2,0.01,0.5,1)
+piParam = PIParameters(10,0.01,0.01,0.8,1)
 
 pi = PI(piParam)
 
