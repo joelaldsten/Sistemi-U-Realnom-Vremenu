6112a044644ed63f296bfa1914e7f12d16d51610
joelaldsten <aldsten.joel@gmail.com>
2023-05-10 12:09:33 +0200

    xddd

--- Controller.py
+++ Controller.py
@@ -161,7 +161,7 @@ class CrazyLogger:
     
 def start_com(q, reg):
     s = socket.socket()
-    s.connect(("192.168.0.105", 55555))
+    s.connect(("192.168.0.100", 55555))
     print("Connected")
     while True:
         data = s.recv(1024).decode("utf-8")
