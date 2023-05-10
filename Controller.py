b169c71cacd85f28c65a0f7ecd123722b4043a8b
joelaldsten <aldsten.joel@gmail.com>
2023-05-10 14:37:41 +0200

    xdddd

--- Controller.py
+++ Controller.py
@@ -194,11 +194,6 @@ uri = uri_helper.uri_from_env(default='usb://0') # Connection-uri for crazyflie 
 cl = CrazyLogger(uri)
 time.sleep(1) # Wait for connection to work
 
-servo_contr.actuate(100,100,100)
-time.sleep(1)
-servo_contr.actuate(-105, 700, -595)
-time.sleep(1)
-servo_contr.actuate(0,0,0)
 q = queue.Queue()
 pi = PI(PIParameters(1.25,5,0.005,0.6,50))
 reg = Regul(q, pi, 0.1, servo_contr, cl)
